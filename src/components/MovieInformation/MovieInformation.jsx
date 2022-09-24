ef91ccd0a8d5d0837efd4d77e2e30b21d3f4f02d
Dani <leonte.daniel37@gmail.com>
2022-09-24 21:38:45 +0300

    file and folder structure

---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MovieInformation = () => {
+  console.log('Movie information');
+
+  return (
+    <div>
+      MovieInformation
+    </div>
+  );
+};
+
+export default MovieInformation;
