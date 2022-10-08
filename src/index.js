96805336a228638a93e15df115060f5cc3144c65
Dani <leontedaniel37@gmail.com>
2022-10-08 11:07:44 +0300

    First version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import ToggleColorMode from './utils/ToggleColorMode';
 import App from './components/App';
+import store from './app/store';
+import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <ToggleColorMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ToggleColorMode>
+  </Provider>,
   document.getElementById('root'),
 );
